a0ab6aef4a5019ea299aeaed25a03446eb68febb
DilbaniE <claudiaenriquez241@gmail.com>
2025-05-05 21:28:53 -0500

    loops while!

--- movies/src/main/java/org/example/Evaluaciones.java
+++ movies/src/main/java/org/example/Evaluaciones.java
@@ -2,19 +2,25 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Loops {
+public class Evaluaciones {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nota = 0;
         double media = 0;
+        double total = 0;
 
-        for (int i = 0; i < 3; i++) {
+        while (nota != -1) {
             System.out.println("Escribe la nota que le daras a la pelicula Matriz");
             nota = sc.nextDouble();
-            media = media + nota;
+            if (nota != -1 ) {
+                media += nota;
+                total++;
+            }
             System.out.println("la nota es " + media);
 
         }
-        System.out.println("la media de evaluacion para Matriz es: " + media / 3);
+        System.out.println("la media de evaluacion para Matriz es: " + media / total);
+
     }
 }
+
